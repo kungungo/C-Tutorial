0b638086604fe0adea9ba6792a3136b9ba83ada4
Vincent Z <zhuvinc8490@gmail.com>
2016-05-07 09:52:56 -0700

    Added files via upload
    Student Grade Simulator and Random Number Generator example

--- main.cpp
+++ main.cpp
@@ -1,105 +1,50 @@
 #include <iostream>
-#include <sstream>
-#include <string>
+#include <cstdlib>
+#include <ctime> //time function included
 
 using namespace std;
 
-//Base class
-class Employee{
-    protected://accessible by derived class (if private only within Employee)
-        string name; //only accessible within Employee otherwise change it to protected
-        double pay;
-    public:
-        Employee(){ //default constructor
-            name = "";
-            pay = 0;
-        }
-        Employee(string empName, double payRate){ //proper constructor
-            name = empName;
-            pay = payRate;
+void random_gen(){
 
-        }
-        //class methods
-        string getName(){
-            return name;
-        }
-        void setName(string empName){
-            name = empName;
-        }
-        double getPay(){
-            return pay;
-        }
-        void setPay (double payRate){
-            pay = payRate;
-        }
-        string toString(){
-            stringstream stm;
-            stm << name << ": " <<pay;
-            return stm.str();
-        }
-        double grossPay(int hours){
-            return hours * pay;
-        }
-};
+    srand(time(0));
+    double grades[10];
+    double random;
+    double total = 0;
+    double average = 0;
+    for(int i = 0; i<10; i++){
+        random = 50 + (rand() % 50) + 0.01*(rand() % 100);
+        grades[i] = random;
+        total = total + grades[i];
+    }
 
-//Derived class (manager in this case derives the employee class public attributes such as name and payrate)
-class Manager : public Employee{
-    private:
-        bool salaried;
+    for(int i = 0; i<10; i++){
+        cout<<"Student's test #: "<<i+1<<" has a score of: "<<grades[i]<<endl;
 
-    public:
-        Manager(string name, double payRate, bool isSalaried) : Employee(name, payRate)
-        { // call to base constructor
-            salaried = isSalaried;
+        if(grades[i]>=90){
+            cout<<"A grade, excellent!"<<endl<<endl;
         }
-        bool getSalaried(){
-            return salaried;
+        else if(grades[i]>=80 && grades[i]<90){
+            cout<<"B grade, well done."<<endl<<endl;
         }
+        else if(grades[i]>=70 && grades[i]<80){
+            cout<<"C grade, passed."<<endl<<endl;
+        }
+        else if(grades[i]>=60 && grades[i]<70){
+            cout<<"D grade, barely passing..."<<endl<<endl;
+        }
+        else{
+            cout<<"F grade, failed."<<endl<<endl;
+        }
+    }
 
-        double grossPay(int hours){ //pays different to regular employee, different method than to Employee Class!
-            if(salaried){
-                return pay;
-            }
-            else{
-                return pay * hours;
-            }
-        }
+    average = total/(sizeof(grades)/sizeof(grades[0]));
 
-        string toString(){
-            stringstream stm;
-            string salary;
-            if(salaried){
-                salary = "Salaried";
-            }
-            else{
-                salary = "Hourly";
-            }
-            stm << name << ": $" <<pay << " : "<<salary<<endl;
-            return stm.str();
-        }
-
-};
+    cout<<"Student's average score on exams: "<<average<<endl;
+}
 
 
 int main()
 {
-
-//    Employee emp1("Jane Smith", 35000);
-//    Employee emp2("Bill Brown", 32000);
-//    cout<<emp1.toString() <<endl;
-//    cout<<emp2.toString() <<endl;
-
-    Employee emp1("Vincent Z", 15.00);
-    cout<<"Employee name: " <<emp1.getName()<<endl;
-    cout<<"Employee pay rate: "<<emp1.getPay()<<endl;
-    cout<<"Employee gross pay: $"<<emp1.grossPay(40)<<endl;
-    Manager emp2("Vince's Manager", 1500, true);
-    cout<<"Employee 2 name: " <<emp2.getName()<<endl;
-    cout<<"Employee 2 pay rate: "<<emp2.getPay()<<endl;
-    cout<<"Employee 2 salaried? "<<emp2.getSalaried()<<endl;
-    cout<<emp2.toString();
-    cout<<"Employee 2 gross pay: $"<<emp2.grossPay(40)<<endl;
-
-
+    random_gen();
     return 0;
 }
